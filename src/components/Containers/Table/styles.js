0f2f930d140b646d689439d2acb03b777a6806a5
Marceometry <marceometry@gmail.com>
2021-10-06 16:24:07 -0300

    limit table width

--- src/components/Containers/Table/styles.js
+++ src/components/Containers/Table/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const Table = styled.table`
   width: 100%;
+  max-width: 1080px;
   border-collapse: collapse;
   border: 1px solid ${({ theme }) => theme.borderColor};
   background: ${({ theme }) => theme.secondaryBackground};
